6552ff8630fc97395f627d7601bf49142b8f0b54
kalim90 <kxl5072@gmail.com>
2018-09-23 03:35:23 +0900

    fix allowed host:i

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fpkkb0w-=8p*8&^!7zbq9h%a*vvwc&s!a%p+#qks+4*@j7oop$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
